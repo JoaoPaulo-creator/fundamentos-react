be5aead019d9a2b27078f91634154b016bffd9a6
JoaoPaulo-creator <joaosarto@gmail.com>
2023-01-02 22:22:58 -0300

    Pequeno ajuste nos styles do post

--- src/App/components/posts/styles.js
+++ src/App/components/posts/styles.js
@@ -3,14 +3,15 @@ import styled, { css } from "styled-components";
 
 export const Container = styled.article`
   margin-bottom: 24px;
-  /* opacity: ${(props) => props.removed ? 0.5 : 1};
-  color: ${(props) =>  props.removed ? '#f00' : '#fff'}; */
+  opacity: ${(props) => props.removed ? 0.5 : 1};
+  color: ${(props) =>  props.removed ? '#f00' : '#fff'};
 
   /* Alternativa para usar, chamar a function css do styled components, para juntar o código repetitivo num lugar só */
 
-  ${(props) => css`
+  /* ${(props) => css`
     opacity: ${props.removed ? 0.5 : 1};
-    color: ${props.removed ? '#f00' : '#fff'};`};
+    color: ${props.removed ? '#f00' : '#fff'};
+  `}; */
 `
 
 export const Subtitle = styled.small`
